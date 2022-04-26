3224db980203ae65202208a89b93d3fc1c78d904
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-04-26 08:47:37 -0500

    added exports to all classes. Completed manager.test.js. Tested Manager.js and it is running as expected

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -4,9 +4,19 @@ describe('Employee', () => {
     // Creates the constructor
     const testName = 'Tim';
     const testId = '26';
-    const testEmail = "test@email.com"
+    const testEmail = "test@email.com";
 
     const constructor = new Employee(testName, testId, testEmail);
+
+    // Confirming constructor builds as expected.
+    describe('constructor', () => {
+        it('should take in name, id, email and return an object', () => {
+            const desired = {name: 'Tim', id: '26', email: 'test@email.com'};
+            const result = constructor;
+    
+            expect(result).toEqual(desired);
+        });
+    });
 
     // Confirming the values can be pulled and returned after the constructor
     describe('getName', () => {
--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,1 +1,32 @@
-const Manager = require("./lib/Manager");
+const Manager = require("../lib/Manager.js");
+
+describe('Manager', () => {
+    // Creates the constructor
+    const testName = 'Tim';
+    const testId = '26';
+    const testEmail = "test@email.com";
+    const testOfficeNum = '555-555-5555';
+
+    const constructor = new Manager(testName, testId, testEmail, testOfficeNum);
+
+    // Confirming constructor builds as expected.
+    describe('constructor', () => {
+        it('should take in name, id, email and return an object', () => {
+            const desired = {name: 'Tim', id: '26', email: 'test@email.com', officeNum: '555-555-5555'};
+            const result = constructor;
+    
+            expect(result).toEqual(desired);
+        });
+    });
+
+    // Confirming the values can be pulled and returned after the constructor
+    describe('getRole', () => {
+        it('should get and return Manager title', () => {
+            const desired = 'Manager';
+            const result = constructor.getRole(test);
+    
+            expect(result).toEqual(desired);
+        });
+    });
+
+});
