f45c6e2a5f8c9aba18c2e9b84d770b10b181c5d8
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-04-26 23:21:31 -0500

    able to get role from 'of loop'

--- README.md
+++ README.md
@@ -1,24 +1,29 @@
-# Note Taker
+# Team Builder
 UT Bootcamp Homework #10 - Object Oriented Programing
 
 ## Description:
-
+A simple team builder that takes in manager information, and allows the manager to add team members. Once added, the information will export as an html file.
 
 ## How to Use:
-
+Enter in the manager's information <br />
+Then select either: 'Add Engineer', 'Add Intern', or 'Stop building team' <br />
+If you choose either Add Engineer or Add Intern, follow the prompts to populate their information <br />
+Choose 'Stop building team' to send your data to the HTML
 
 ## Website Visual Demo(Gif/video):
 ![Banner](./demos/demo_passingTests.gif) <br />
 <a href="">Video Demo</a>
 
 ## Link to Deployed Page:
-
+N/A
 
 ## References:
-Test code inspired by UT Web Dev Bootcamp: UTA-VIRT-FSF-FT-03-2022-U-LOL
+Test code inspired by UT Web Dev Bootcamp: UTA-VIRT-FSF-FT-03-2022-U-LOL <br />
+Array storage inspired by: <a href="https://github.com/yummy314159265">Rodin</a>
 
 ## Development Process (See commits for more details):
 Created base files and outline <br />
-Pseudocoded based off acceptance criteria/README.md 
-Tested classes using Jest
-Continued after passing all tests
+Pseudocoded based off acceptance criteria/README.md <br />
+Tested classes using Jest <br />
+Continued after passing all tests <br />
+Created input using inquirer <br />
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Node.js command line application
 const inquirer = require('inquirer-promise');
+const fs = require('fs');
 
 // Classes
 const Employee = require("./lib/employee");
@@ -7,17 +8,24 @@ const Manager = require("./lib/manager");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 
-const team = [];
+let team = [];
 let needTeamMembers = true;
+
+// Creates elements
+
 
 // Creates all aspects of team
 async function createTeam() {
     // Gets manager info
-    // await createManager();
+    await createManager();
 
     // Adds team members
     await addTeamMembers();
-    console.log(team);
+
+    // Future Dev: Delete Option
+
+    // Starts HTML Generation
+    generatreHTML();
 }
 
 // Adds team members
@@ -46,7 +54,6 @@ async function addTeamMembers() {
             await addIntern();
         }
     }
-
 }
 
 async function createManager() {
@@ -150,42 +157,48 @@ async function addIntern() {
         })     
 }
 
+// Processes obtained data 
+function generatreHTML() {
+    // Go through all the options in the array.
+    for (const value of team) {
+        console.log(value);
+        console.log(value.getRole());
+    }
+    // Be able to find data types based on class
+    // Append each of these to the main
+    // Export all of one class, then go on to the next
+    // Export Manager
+    // Export Engineer
+    // Export Intern
 
-
-
+}
 // Start application
 function init() {
     createTeam();
+
 }
 init();
-
-
-
-// Presented with a menu with the option to add an engineer or an inter, or finish building my 
-
-// Takes info about employees - 
-// Consider adding a validation to ensure user input is the proper format.
-// Bonus: Add a delete function
-
-
-
-// Prompt getting team member names and info (emails/github)
-// If employee is an enginneer get: Name, ID, email, and github, username.
-// If employee is an intern get: Name ID, email, and school
-// After submitting an employee, I am taken back to the menu (to add/etc)
-// After finished building, generate HTML file.
-
-
-// Generate HTML file displaying a nicely formated team roster
-// Webpage that displays summaries for each person. (Name, ID, Email, github/school)
 
 // HTML functionality
 // Click on an email address => default email program opens
 // Click on github name => new tab for github repo
 
+// // Save data locally from input
+// function saveData () {
+//     // Stores new data into the object
+//     fs.writeFile("teamData.json", JSON.stringify(team), (err) =>
+//     err ? console.error(err) : console.log('Success!'));
+// }
 
-
-// Write a unit test for every part of your code
-// Create a.test JS file for each js excluding index.js
-
-
+// // loads local data if present
+// function loadData () {
+//     // Stores new data into the object
+//     let teamData = [];
+//     fs.readFile("teamData.json", 'utf8', (err,data) => {
+//         if (!err) {
+//             teamData=JSON.parse(data);
+//             team = teamData;
+//             generatreHTML()
+//         }
+//     });
+// }
