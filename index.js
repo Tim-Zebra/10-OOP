1c0b2deb3fdff4108ade6f8ae105a3e3eb55f720
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-04-26 20:08:47 -0500

    added init function. Added Build team function

--- index.js
+++ index.js
@@ -7,21 +7,17 @@ const Manager = require("./lib/manager");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 
-// Start application
-// Take in managers name, employee ID, email, and office #
-// Presented with a menu with the option to add an engineer or an inter, or finish building my 
-inquirer
-  .prompt([
+// Manager Prompts
+const managerPrompts = [
     {
-      type: 'input',
-      message: 'What is the manager\'s name?',
-      name: 'manName',
+        type: 'input',
+        message: 'What is the manager\'s name?',
+        name: 'name',
     },
-
     {
-      type: 'input',
-      message: 'What is your employee ID?',
-      name: 'empId',
+        type: 'input',
+        message: 'What is your employee ID?',
+        name: 'empId',
     },
     {
         type: 'input',
@@ -32,12 +28,34 @@ inquirer
         type: 'input',
         message: 'Please enter your office phone number',
         name: 'phoneNum'
-    }
-  ])
-  .then((response) => {
-      responses = [response.manName, response.empId, response.email];
-      console.log(responses);
-    });
+    }];
+
+
+// Take in manager's info
+function createTeam() {
+    let team = {};
+    inquirer
+        .prompt(managerPrompts)
+        .then((response) => {
+            let employee = new Manager(response.name, response.empId, response.email, response.phoneNum);
+            console.log(employee);
+            console.log(typeof employee);
+            let title = employee.getRole();
+            team[title] = employee;
+
+            console.log(team);
+        });
+}
+
+// Start application
+function init() {
+    createTeam();
+}
+init();
+
+
+
+// Presented with a menu with the option to add an engineer or an inter, or finish building my 
 
 // Takes info about employees - 
 // Consider adding a validation to ensure user input is the proper format.
